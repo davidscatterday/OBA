4253eff8b01c1317ad0af325311857cf92a6dafd
David Scatterday <david.scatterday@gmail.com>
2025-01-17 13:14:58 -0500

    udpated time

--- condition-test-10b.py
+++ condition-test-10b.py
@@ -24,7 +24,7 @@ def run_scraper():
     print("schedule scrapper")
     scraper()
 
-schedule.every().day.at("13:12").do(run_scraper)
+schedule.every().day.at("13:15").do(run_scraper)
 
 
 scheduler_thread = threading.Thread(target=run_scheduler, daemon=True)
